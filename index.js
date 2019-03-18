f61528a75be3f616a1248f91303b11f14fbbcab4
Ken Zhou <mailofrealcev@gmail.com>
2019-03-17 21:25:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ $("#play").click(()=>{
   $("#home").hide();
   $("#game").show();
 });
+
+function Game () {
+  this.Box = function (text) {
+    this.text = text;
+    $('#box').innerText = text;
+  }
+}
+
+var game = new Game();
+
+var box = new game.Box('yeet it is a box');
