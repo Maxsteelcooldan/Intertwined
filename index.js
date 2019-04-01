78be7e410b4b19fde464f7fdb14b35720a287ab5
Craftingexpert1 <34014055+Craftingexpert1@users.noreply.github.com>
2019-04-01 19:30:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -30,13 +30,10 @@ var game = new Game();
 var box = new game.box('In a world where mankind has been turned into non-living objects.The only person who can save the world is the Block');
 
 var main = document.querySelector('#main')
-$(document).keydown(function(e) {
-var position = $("main").position();
-
-  switch (e.keyCode)
-  {
-    case 87:
-   $("#main").css('top', position.top - 2 + 'px');
-   break;
+$(document).keydown((e)=>{
+  var position = $("main").position();
+  key=e.key.toLowerCase();
+  if(key=='w'){
+    $("#main").css('top', position.top - 2 + 'px');
   }
 });
