df83fca2fdfd98342510bed4bf923f737b1bbf87
Maxsteelcooldan <46035576+Maxsteelcooldan@users.noreply.github.com>
2019-04-04 22:46:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ $('#box').text('In a world where mankind has been turned into non-living objects
 var game = new world();
 $('#levels').append(game.el);
 game.el.style.background = "#42f4d7";
-var ground =new game.rect(10,10,30,30,0,'green');
+var ground =new game.rect(1,100,30,30,0,'green');
 var player=new game.critter(0,0,50,50,0,'<img src="Block.png" style="width:100%">','player');
 var key={};
 document.onkeypress=(e)=>key[e.key.toLowerCase()]=e.type=true;
