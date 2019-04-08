6f32cc6f8197c4cc05630ab61550443b7c60e0fa
Maxsteelcooldan <46035576+Maxsteelcooldan@users.noreply.github.com>
2019-04-08 19:59:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -69,30 +69,30 @@ controller = {
 
 loop = function() {
 
-  if (controller.up && rectangle.jumping == false) {
+  if (controller.up && player.jumping == false) {
 
-    rectangle.y_velocity -= 20;
-    rectangle.jumping = true;
+    player.y_velocity -= 20;
+    player.jumping = true;
 
   }
 
   if (controller.left) {
 
-    rectangle.x_velocity -= 0.5;
+    player.x_velocity -= 0.5;
 
   }
 
   if (controller.right) {
 
-    rectangle.x_velocity += 0.5;
+    player.x_velocity += 0.5;
 
   }
 
-  rectangle.y_velocity += 1.5;// gravity
-  rectangle.x += rectangle.x_velocity;
-  rectangle.y += rectangle.y_velocity;
-  rectangle.x_velocity *= 0.9;// friction
-  rectangle.y_velocity *= 0.9;// friction
+  player.y_velocity += 1.5;// gravity
+  player.x += player.x_velocity;
+  player.y += player.y_velocity;
+  player.x_velocity *= 0.9;// friction
+  player.y_velocity *= 0.9;// friction
 
   // if rectangle is falling below floor line
   if (rectangle.y > 132) {
@@ -104,13 +104,13 @@ loop = function() {
   }
 
   // if rectangle is going off the left of the screen
-  if (rectangle.x < -32) {
+  if (player.x < -32) {
 
-    rectangle.x = 320;
+    player.x = 320;
 
-  } else if (rectangle.x > 320) {// if rectangle goes past right boundary
+  } else if (player.x > 320) {// if rectangle goes past right boundary
 
-    rectangle.x = -32;
+    player.x = -32;
 
   }
 
@@ -118,7 +118,6 @@ loop = function() {
   context.fillRect(0, 0, 320, 180);// x, y, width, height
   context.fillStyle = "#ff0000";// hex for red
   context.beginPath();
-  context.rect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
   context.fill();
   context.strokeStyle = "#202830";
   context.lineWidth = 4;
