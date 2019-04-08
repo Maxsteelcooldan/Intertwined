9ec711c9b886582aa69deb3d89e909469c5810d6
Maxsteelcooldan <46035576+Maxsteelcooldan@users.noreply.github.com>
2019-04-08 17:16:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -19,15 +19,15 @@ $("#one").click(()=>{
   $("#levels").show();
 });
 
-var context, controller, rectangle, loop;
+var context, controller, player, loop;
 
 context = document.querySelector("canvas").getContext("2d");
+player = document.querySelector("player");
 
 context.canvas.height = 180;
 context.canvas.width = 320;
 
-rectangle = {
-
+player = {
   height:32,
   jumping:true,
   width:32,
@@ -35,13 +35,11 @@ rectangle = {
   x_velocity:0,
   y:0,
   y_velocity:0
-
 };
 
 controller = {
-
-  left:false,
   right:false,
+  left:false,
   up:false,
   down:false,
   keyListener:function(event) {
@@ -50,16 +48,16 @@ controller = {
 
     switch(event.keyCode) {
 
-      case 37:// left key
-        controller.left = move;
-      break;
-      case 38:// up key
+      case 87:// w key
         controller.up = move;
       break;
-      case 39:// right key
+      case 65:// a key
+        controller.left = move;
+      break;
+      case 68:// d key
         controller.right = move;
       break;
-      case 39:// s key
+      case 83:// s key
         controller.down = move;
       break;
 
@@ -71,35 +69,26 @@ controller = {
 
 loop = function() {
 
-  if (controller.up && rectangle.jumping == false) {
-
+  if (controller.up && player.jumping == false) {
     rectangle.y_velocity -= 20;
     rectangle.jumping = true;
-
   }
-
   if (controller.left) {
-
-    rectangle.x_velocity -= 0.5;
-
+    player.x_velocity -= 0.5;
   }
 
   if (controller.right) {
-
-    rectangle.x_velocity += 0.5;
-
+    player.x_velocity += 0.5;
   }
   
   if (controller.down) {
-    
-    rectangle.x_velocity += 20;
-  
+    player.x_velocity += 20;
   }
-  rectangle.y_velocity += 1.5;
-  rectangle.x += rectangle.x_velocity;
-  rectangle.y += rectangle.y_velocity;
-  rectangle.x_velocity *= 0.9;
-  rectangle.y_velocity *= 0.9;
+  player.y_velocity += 1.5;
+  player.x += player.x_velocity;
+  player.y += player.y_velocity;
+  player.x_velocity *= 0.9;
+  player_velocity *= 0.9;
 
   window.requestAnimationFrame(loop);
 
