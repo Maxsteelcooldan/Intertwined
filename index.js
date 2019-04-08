a111a647f933dd9d3d1db98c7f0a8c7dacc7cbb5
Maxsteelcooldan <46035576+Maxsteelcooldan@users.noreply.github.com>
2019-04-08 16:45:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,30 @@ context = document.querySelector("canvas").getContext("2d");
 context.canvas.height = 180;
 context.canvas.width = 300;
 
-player = document.querySelector("player")
+player = document.querySelector("player");
+
+player {  
+  height:32,
+  jumping:true,
+  width:32,
+  x:144, // center of the canvas
+  x_velocity:0,
+  y:0,
+  y_velocity:0
+};
+
+controller {
+  up:false,
+  down:false,
+  right:false,
+  left:false,
+  keyListener:function(event) {
+  var move = (event.type == "keycode")?true:false
+  }
+}
 
 
 
+window.addEventListener("keydown", controller.keyListener)
+window.addEventListener("keyup", controller.keyListener);
+window.requestAnimationFrame(loop);
