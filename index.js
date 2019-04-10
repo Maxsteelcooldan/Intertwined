77677fb1930257852560e312924fbea3cab74689
Craftingexpert1 <34014055+Craftingexpert1@users.noreply.github.com>
2019-04-10 12:36:57 -0400

    Update index.js

--- index.js
+++ index.js
@@ -24,11 +24,11 @@ context = document.querySelector("#canvasone").getContext("2d");
 context.canvas.height = window.innerHeight;
 context.canvas.width = window.innerWidth;
 
-var rectangle = new Player(144, 144, 144, 0);
+var rectangle = new Player(144, 144, 18, 0); // making the player.
 
-function Player(width, height, x, y) {
+function Player(width, height, x, y) { // function for making a Player.
   var that = this;
-  this.width = 18;
+  this.width = width;
   this.height = height;
   this.x = x;
   this.y = y;
@@ -47,74 +47,49 @@ function World(controller, width, height, gravity, friction, floor_height, playe
   this.controller = controller;
   this.player = player;
   this.animationLoop = function () {
-
     if (that.controller.up && that.player.jumping == false) {
-
       that.player.y_velocity -= 80;
       that.player.jumping = true;
-
     }
-
     if (that.controller.left) {
-
       that.player.x_velocity -= 0.5;
-
     }
-
     if (that.controller.right) {
-
       that.player.x_velocity += 0.5;
-
     }
-
     that.player.y_velocity = that.player.y_velocity + that.gravity;
     that.player.x_velocity *= that.friction;// friction
     that.player.y_velocity *= that.friction;// friction
     that.player.x += that.player.x_velocity;
     that.player.y += that.player.y_velocity;
-
     // if that.player is falling below floor line
     if (that.player.y > that.height - that.floorHeight - that.player.height / 2 + 10) {
-
       that.player.jumping = false;
       that.player.y = that.height - that.floorHeight - that.player.height / 2 + 10;
       that.player.y_velocity = 0;
-      
     }
-
     // if that.player is going off the left of the screen
     if (that.player.x < -32) {
-
       that.player.x = that.width + 32;
-
     } else if (that.player.x > width + 32) {// if that.player goes past right boundary
-
       that.player.x = -32;
-
     }
     image = new Image();
     image.src = "Block.png";
     context.fillStyle = "#2196F3";
-    context.fillRect(0, 0, that.width, that.height); // aneesh, please label your code with comments.
-    context.beginPath();
+    context.fillRect(0, 0, that.width, that.height);//clear rect with blue
+    context.beginPath(); //start drawing
     context.fillStyle = "#4caf50";
-    context.rect(0, that.height - that.floorHeight, that.width, that.floorHeight, 'grey')
+    context.rect(0, that.height - that.floorHeight, that.width, that.floorHeight, 'grey');
     context.drawImage(image, that.player.x, that.player.y, that.player.width, that.player.height);
     context.fill();
-
-    // call update when the browser is ready to draw again
-    window.requestAnimationFrame(that.animationLoop);
-
+    window.requestAnimationFrame(that.animationLoop); // when it is able to loop again
   };
 
   this.start = function () {
     document.onkeypress = that.controller.keyListener;
     document.onkeyup = that.controller.keyListener;
     window.requestAnimationFrame(that.animationLoop);
-    //document.onclick = function (e) {
-     // that.player.x = e.clientX - 30;
-     // that.player.y = e.clientY - 30;
-    }
   };
 }
 
