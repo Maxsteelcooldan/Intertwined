468cdcfc6b3d030842197353e4736d7ef616e2b2
Maxsteelcooldan <46035576+Maxsteelcooldan@users.noreply.github.com>
2019-05-09 18:26:13 -0400

    Update phaser.js

--- phaser.js
+++ phaser.js
@@ -1,1 +1,26 @@
+const hero = {}
+
+const config = {
+  type: Phaser.AUTO,
+  width: 400,
+  height: 450,
+  backgroundColor: 0x333333,
+  scene: {
+    create,
+    preload,
+    update
+  }
+}
+
+const game = new Phaser.Game(config)
+
+function preload() {
+  this.load.image('block', 'assets/block.png' )
+}
+function create() {
+  hero.main = this.add.sprite(100,400, 'block')
+}
+function update() {
+
+}
 
