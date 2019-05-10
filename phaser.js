94f2ba8fb19ffa9131917e4a509b6bba4388d4f3
Maxsteelcooldan <46035576+Maxsteelcooldan@users.noreply.github.com>
2019-05-09 22:26:31 -0400

    Update phaser.js

--- phaser.js
+++ phaser.js
@@ -19,8 +19,14 @@ function preload() {
 }
 function create() {
   hero.main = this.add.sprite(100,400, 'block')
+  hero.cursors = this.input.keyboard.createCursorKeys();
 }
 function update() {
-
+  if(hero.cursors.right.isDown ) {
+    hero.main.x += 5
+  } else if (hero.cursors.left.isDown) {
+    hero.main.x -= 5
+  } else if (hero.cursors.up.isDown) {
+    hero.main.y += 5
+  }
 }
-
