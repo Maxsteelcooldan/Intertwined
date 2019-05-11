df28d133363cb845c32491a41449825757a0a9ad
Maxsteelcooldan <46035576+Maxsteelcooldan@users.noreply.github.com>
2019-05-11 10:28:34 -0400

    Update phaser.js

--- phaser.js
+++ phaser.js
@@ -1,32 +1,48 @@
 const hero = {}
+var keyA, keyW, keyD
 
 const config = {
   type: Phaser.AUTO,
-  width: 400,
-  height: 450,
-  backgroundColor: '#2196F3',
+  width: 450,
+  height: 500,
+  backgroundColor: "b9eaff",
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 },
+      enableBody: true,
+    }
+  },
   scene: {
-    create,
     preload,
+    create,
     update
   }
 }
 
+
 const game = new Phaser.Game(config)
 
 function preload() {
-  this.load.image('block', 'Block.png' )
+  this.load.image('block', 'assets/block.png' )
+  this.load.image('ground', 'assets/block.png')
 }
 function create() {
-  hero.main = this.add.sprite(100,400, 'block')
+  hero.main = this.physics.add.sprite(100,400, 'block')
   hero.cursors = this.input.keyboard.createCursorKeys();
+  hero.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+  hero.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+  hero.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+  hero.main.setCollideWorldBounds(true)
+  platforms = this.physics.add.staticGroup();
+  platforms.create(400, 568, 'ground').setScale(2).refreshBody();
 }
 function update() {
-  if(hero.cursors.right.isDown ) {
+  if(hero.cursors.right.isDown || hero.keyD.isDown ) {
     hero.main.x += 5
-  } else if (hero.cursors.left.isDown) {
+  } else if (hero.cursors.left.isDown || hero.keyA.isDown) {
     hero.main.x -= 5
-  } else if (hero.cursors.up.isDown) {
+  } else if (hero.cursors.up.isDown || hero.keyW.isDown) {
     hero.main.y -= 5
-  }
+  } 
 }
