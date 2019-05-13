10fc933ae1a1f565413e65e7c6e76f07ec0ae32c
Maxsteelcooldan <46035576+Maxsteelcooldan@users.noreply.github.com>
2019-05-13 16:34:34 -0400

    Update phaser.js

--- phaser.js
+++ phaser.js
@@ -23,7 +23,7 @@ function preload() {
   this.load.image('starrynight', 'starrynight.jpg')
 }
 function create() {
-  hero.image = this.add.tileSprite(60,50,0,0  'starrynight')
+  hero.image = this.add.tileSprite(60,50,0,0,  'starrynight')
   hero.main = this.physics.add.sprite(100,400, 'blockimage')
   hero.cursors = this.input.keyboard.createCursorKeys();
   hero.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
